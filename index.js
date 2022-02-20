99e5d4bdfb98dd8e97f1ec891f3a89d70fcc437c
Caio Gottmann <caio.gottmann@dtidigital.com.br>
2022-02-19 21:00:31 -0300

    Codigo com bfs

--- index.js
+++ index.js
@@ -1,256 +1,182 @@
-class Peca {
-  constructor(location, row, column, peso, visited = false) {
-    this.location = location; // posicao atual
-    this.row = row; //
-    this.column = column; //
-    this.peso = peso; // valor da peça
-    this.visited = visited;
-    this.parents = {
-      up: null,
-      down: null,
-      left: null,
-      rigth: null,
-    };
+let initialState;
+const goalState = [1, 2, 3, 8, 0, 4, 7, 6, 5];
+
+class BFS {
+  constructor(initialState, goalState) {
+    this.initialState = initialState;
+    this.goalState = goalState;
+    this.allNodes = {};
+    this.visitedNodes = [];
+    this.Queue = [];
+    // ADD INITAL TO QUEUE
+    this.Queue.push(initialState);
   }
 
-  updatePosition(newPosition) {
-    this.position = newPosition;
+  // MOVE FUNCTIONS
+  swap(value, swapfrom, swapto) {
+    const temp = value[swapto];
+    value[swapto] = value[swapfrom];
+    value[swapfrom] = temp;
+    return value;
   }
-}
 
-var vetPieces;
-let blankLocation;
-
-function insertValues() {
-  var table = document.getElementById("emptyTable");
-  let valorTeste = document.querySelectorAll("[location]");
-
-  //for(let i=0;i<valorTeste.length;i++){
-  //  console.log(valorTeste[i].getAttribute("location") +'valor dentro' + valorTeste[i].innerText)
-
-  let cont = 0;
-  for (var j = 0; j < 3; j++) {
-    for (var k = 0; k < 3; k++) {
-      let locationaux = valorTeste[k + j + cont].getAttribute("location");
-      if (valorTeste[cont + k + j].innerText.length == 0) {
-        console.log(valorTeste[cont + k + j].innerText);
-        vetPieces[j][k] = new Peca(locationaux, j, k, 0);
-        blankLocation = [j, k];
-      } else {
-        console.log(valorTeste[cont + k + j].innerText);
-        vetPieces[j][k] = new Peca(
-          locationaux,
-          j,
-          k,
-          valorTeste[cont + k + j].innerText
-        );
-      }
+  //   initialState[0].innerText
+  checkGoalState(value) {
+    // value is array
+    if (JSON.stringify(goalState).indexOf(JSON.stringify(value)) >= 0) {
+      return true;
+    } else {
+      return false;
     }
-    cont += 2;
   }
+
+  // GET POSSIBLE MOVES
+  getPossibleMoves(value) {
+    let mid = {
+      0: [{ swap: 1 }, { swap: 3 }],
+      1: [{ swap: 2 }, { swap: 4 }, { swap: 0 }],
+      2: [{ swap: 5 }, { swap: 1 }],
+      3: [{ swap: 0 }, { swap: 4 }, { swap: 6 }],
+      4: [{ swap: 1 }, { swap: 5 }, { swap: 7 }, { swap: 3 }],
+      5: [{ swap: 2 }, { swap: 8 }, { swap: 4 }],
+      6: [{ swap: 3 }, { swap: 7 }],
+      7: [{ swap: 4 }, { swap: 8 }, { swap: 6 }],
+      8: [{ swap: 5 }, { swap: 7 }],
+    };
+
+    return mid[value];
+  }
+
+  mov(visited) {
+    const locationResult = document.querySelectorAll("[locationResult]");
+    locationResult.forEach((item, index) => {
+      if (visited[index] == 0) {
+        item.querySelector("p").innerText = "";
+        item.querySelector("div").setAttribute("class", "squareResult vazio");
+      } else {
+        item.querySelector("p").innerText = visited[index];
+        item.querySelector("div").setAttribute("class", "squareResult");
+      }
+    });
+  }
+
+  sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  // initialState.find((item) => item.innerText == '')
+  main = async function () {
+    let index = this.Queue[0].indexOf(0);
+    let moves = this.getPossibleMoves(index);
+
+    // GET CHILDS
+    let childNodes = moves.map((item) => {
+      let temp = [...this.Queue[0]];
+      return this.swap(temp, index, item.swap);
+    });
+
+    childNodes.forEach((item) => {
+      if (JSON.stringify(this.Queue).indexOf(JSON.stringify(item)) == -1) {
+        // IF NOT FOUND ON QUEUE THEN INSERT IN QUEUE AND NOT IN VISITED
+        if (
+          JSON.stringify(this.visitedNodes).indexOf(JSON.stringify(item)) == -1
+        ) {
+          this.Queue.push(item);
+        }
+      }
+    });
+
+    let visited = this.Queue.splice(0, 1)[0];
+    this.visitedNodes.push(visited);
+
+    this.mov(visited);
+    await this.sleep(100);
+    // this.mov(this.initialState);
+
+    if (this.checkGoalState(visited)) {
+      // this.mov(visited);
+      console.log("FOUND");
+
+      const valorSucess = document.getElementsByClassName("squareResult");
+      for (let index = 0; index < valorSucess.length; index++)
+        valorSucess[index].setAttribute("class", "squareResult sucess");
+
+      // let output = document.getElementById("output");
+
+      // let t = "";
+      // this.visitedNodes.forEach((item) => {
+      //   console.log("passei");
+
+      //   t += '<div class="node">';
+      //   item.forEach((value) => {
+      //     console.log("result", value);
+      //     t += '<div class="node_item">' + value + "</div>";
+      //   });
+      //   t += '<div class="right_arrow">&rarr;</div>';
+      //   t += "</div>";
+      // });
+
+      // output.innerHTML = t;
+
+      console.log(this.visitedNodes.length);
+
+      return;
+    } else {
+      this.main();
+    }
+  };
 }
 
-var vetPecas;
-
-function createMatrix() {
-  // Create one dimensional array
-  var vet = new Array(3);
-
-  // Loop to create 2D array using 1D array
-  for (var i = 0; i < vet.length; i++) {
-    vet[i] = [];
-  }
-  return vet;
-}
-
+//ordem alatoria para a primira tabela
 function ordemAleatoria() {
   const squares = [...document.getElementsByClassName("square")];
   let valorTeste = [...document.querySelectorAll("[location]")];
 
-  console.log(squares, valorTeste);
-  shuffle(squares);
-  shuffle(valorTeste);
-  for (let index = 0; index < 8; index++) {
-    valorTeste[index].append(squares[index]);
-  }
-}
+  // console.log(squares, valorTeste);
+  // shuffle(squares);
+  // shuffle(valorTeste);
+  // for (let index = 0; index < 8; index++) {
+  //   valorTeste[index].append(squares[index]);
+  // }
+  valorTeste[0].append(squares[0]);
+  valorTeste[1].append(squares[1]);
+  valorTeste[2].append(squares[2]);
 
-function ordemAleatorias() {
-  const squares = [...document.getElementsByClassName("squareResult")];
-  let valorTeste = [...document.querySelectorAll("[locationResult]")];
+  valorTeste[5].append(squares[3]);
+  valorTeste[8].append(squares[4]);
+  valorTeste[7].append(squares[5]);
 
-  console.log(squares, valorTeste);
-  shuffle(squares);
-  shuffle(valorTeste);
-  for (let index = 0; index < 8; index++) {
-    valorTeste[index].append(squares[index]);
-  }
-}
+  valorTeste[6].append(squares[6]);
+  valorTeste[4].append(squares[7]);
 
-function shuffle(array) {
-  let randomNumber;
-  let tmp;
-  for (let i = array.length; i; ) {
-    randomNumber = (Math.random() * i--) | 0;
-    tmp = array[randomNumber];
-    // troca o número aleatório pelo atual
-    array[randomNumber] = array[i];
-    // troca o atual pelo aleatório
-    array[i] = tmp;
+  function shuffle(array) {
+    let randomNumber;
+    let tmp;
+    for (let i = array.length; i; ) {
+      randomNumber = (Math.random() * i--) | 0;
+      tmp = array[randomNumber];
+      // troca o número aleatório pelo atual
+      array[randomNumber] = array[i];
+      // troca o atual pelo aleatório
+      array[i] = tmp;
+    }
   }
 }
 
 function Table() {
-  vetPieces = createMatrix();
-  console.log("passei aqui");
   //ordemAleatoria();
-  insertValues();
-  console.log("passei aqui2");
+  renderResult();
 
-  pieceParents();
-
-  agentVerificator();
-  console.log(finalState());
-  //changePieces(vetPieces[0][0],vetPieces[1][0])
-  render();
+  const bfs = new BFS(
+    initialState.map((oi) => parseInt(oi.innerText == "" ? 0 : oi.innerText)),
+    goalState
+  );
+  bfs.main();
 }
 
-// Método que percorre a matrix e atribui os vizinhos
-function pieceParents() {
-  // Implementado
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 3; j++) {
-      pieceParentsAux(vetPieces[i][j]);
-    }
-  }
-}
-
-//Método aux que atribui as peças proximas
-function pieceParentsAux(peca) {
-  //Implementado
-
-  if (peca.row != 0 && peca.row <= 2) {
-    // Nao pode se movimentar para cima
-    peca.parents["up"] = vetPieces[peca.row - 1][peca.column].peso;
-  }
-
-  if (peca.row != 2 && peca.row >= 0) {
-    peca.parents["down"] = vetPieces[peca.row + 1][peca.column].peso;
-  }
-
-  if (peca.column == 0) {
-    // Nao pode se movimentar para cima
-    peca.parents["left"] = null;
-  } else {
-    peca.parents["left"] = vetPieces[peca.row][peca.column - 1].peso;
-  }
-
-  if (peca.column == 2) {
-    // Nao pode se movimentar para baixo
-    peca.parents["rigth"] = null;
-  } else {
-    peca.parents["rigth"] = vetPieces[peca.row][peca.column + 1].peso;
-  }
-}
-
-function verifyFields() {
-  //Função que verifica se os campos estão preenchidos corretamente
-}
-
-//Estado final da máquina no forca bruta
-function finalState() {
-  // Implementado, falta testar
-  let finalStateVector = [1, 2, 3, 8, 0, 4, 7, 6, 5];
-  let count = 0;
-  let countFinalState = 0;
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      if (vetPieces[i][j].peso == finalStateVector[count]) {
-        countFinalState++;
-      }
-      count++;
-    }
-  }
-
-  if (countFinalState == 9) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// Verificar as possiveis posicoes ao qual o mesmo pode andar
-function agentVerificator() {
-  // Implementado, falta testar
-  let parents = vetPieces[blankLocation[0]][blankLocation[1]].parents;
-  console.log(parents);
-  let aux = Object.keys(parents).map(function (key) {
-    return parents[key];
-  });
-
-  let vetParents = aux.filter((obj) => {
-    if (obj != null) {
-      return obj;
-    }
-  });
-
-  return vetParents;
-}
-
-function agentValidator() {
-  // Se tal movimento não foi feito anteriormente.
-}
-
-// Efetivamente vai se movimentar as peças
-function agentRunner() {
-  //Necessita Implementar
-}
-
-//Realiza a troca na matrix entre duas peças
-function changePieces(piece1, piece2) {
-  // Implementado, Falta testar
-
-  let auxiliarPiece = piece1;
-  piece1.row = piece2.row;
-  piece1.column = piece2.column;
-
-  piece2.row = auxiliarPiece.row;
-  piece2.column = auxiliarPiece.column;
-
-  pieceParents();
-}
-
-function visitedPieces(vetAuxiliar) {
-  //erado
-  for (let i = 0; i < vetAuxiliar.length; i++) {
-    if (vetAuxiliar.visited == false) {
-      cont++;
-    }
-  }
-
-  //if(cont == vetAuxiliar.)
-}
-
-// Método de Busca em profundidade
-function hardSearch() {
-  //Implementando
-  let matrixAuxPieces = vetPieces;
-  let vetMovementMatrix;
-  while (!finalState()) {
-    var vetParents = agentVerificator();
-
-    //Ver possibilidade de movimentação
-    //
-    //armazenar movimento
-    //
-  }
-}
-
-function render() {
+function renderResult() {
   let valorTeste = [...document.querySelectorAll("[locationResult]")];
   let locationPadrao = [...document.querySelectorAll("[location]")];
-
   let cont = 0;
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
@@ -266,8 +192,20 @@ function render() {
         } </p>`;
 
         valorTeste[i + j + cont].append(squareResult);
+      } else {
+        let squareResult = document.createElement("div");
+        squareResult.setAttribute("class", "squareResult vazio");
+        squareResult.setAttribute("id", i + j + cont);
+        squareResult.innerHTML = `<p id=
+          -1
+         ></p>`;
+
+        valorTeste[i + j + cont].append(squareResult);
       }
     }
     cont += 2;
   }
+  initialState = [...valorTeste];
 }
+
+ordemAleatoria();
